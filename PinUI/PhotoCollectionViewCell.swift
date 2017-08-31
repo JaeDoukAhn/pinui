90ccfc0fb4ef86192311ebfcd4a119dcdd123f14
JD Ahn <jdahn1914@gmail.com>
2017-08-31 13:14:25 +0900

    dd

---
+++ PinUI/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PinUI
+//
+//  Created by ktds 20 on 2017. 8. 31..
+//  Copyright © 2017년 CJON, Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoimageView: UIImageView!
+}
